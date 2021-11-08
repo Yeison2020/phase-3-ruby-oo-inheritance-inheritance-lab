03b485c92e17bb2280a1886bd9c50b9776347603
Yeison <enmanuelquiterios@gmail.com>
2021-11-08 16:43:30 -0500

    Completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require './user'
+class Student  < User
+   def initialize(knowledge = [])
+    @knowledge = knowledge
+   end 
+
+   def learn string
+    @knowledge.push(string)
+   end
+
+   def knowledge
+    @@knowledge
+   end
+    
+
+
+
+
+
 
 end
